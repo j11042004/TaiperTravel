ca7ab9786ff689d79e210a89245aeaba08c0d0ed
jeff <j11042004@gmail.com>
2024-05-29 21:14:43 +0800

    [新增] 1. gitignore 2.專案初始檔案

---
+++ TaiperTravel/TaiperTravel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaiperTravel
+//
+//  Created by 周佳緯 on 2024/5/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
